d75b68fb7bdbc6c5736651394f8382c6da86b887
Bruxar <larancibia@utem.cl>
2023-11-22 20:13:26 -0300

    Add GitHub icon and link to TarjetaPersona component

--- src/Components/TarjetaPersona/index.jsx
+++ src/Components/TarjetaPersona/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import "./TarjetaPersona.css";
 
-const TarjetaPersona = ({ imagen, nombre, grupo, rol }) => {
+import { BsGithub } from "react-icons/bs";
+
+const TarjetaPersona = ({ imagen, nombre, grupo, rol, github }) => {
   const imagenPorDefecto = 'src/assets/Equipo-img/default.jpg';
 
   // Verificar si la imagen está presente o cargar la imagen por defecto
   const imagenSrc = imagen || imagenPorDefecto;
 
   return (
-    <div className="tarjeta">
+    <a href={github} target="_blank" rel="noopener noreferrer" className="tarjeta">
       <img src={imagenSrc} alt={nombre} />
       <h3>{nombre}</h3>
       <p>{rol}</p>
-    </div>
+      <BsGithub className="github-icon"/>
+    </a>
   );
 };
 
